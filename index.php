e7e145ac42ebbc8e41ddf6f85e98babfa59998e5
taj1uddin <itstaj7272@gmail.com>
2024-01-27 00:57:05 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+    <!-- Über mich -->
+    <section class="about">
+        <div class="bg-orange">
+            <div class="content-section">
+                <div data-aos="fade-up" class="aos-init aos-animate">
+                    <h1 class="rosa">About myself</h1>
+                </div>
+                <div class="row justify-content-center">
+                    <!-- <div class="col-lg-2 aos-init aos-animate" data-aos="fade-up"></div> -->
+                    <div class="col-lg-8 textfeld rosa aos-init aos-animate" data-aos="fade-up" style="width: 723px; max-width: 100%;">
+                        <div class="text">
+                            <p>Made with experience. Hier könnte ich erzählen, dass ich schon Grafikerin werden wollte, ??seit ich mit vier Jahren das erste Mal mit einer Wachsmalkreide die weiße Wand im Wohnzimmer meiner Eltern umgestaltet habe?? Oder dass ich mit über 15 Jahren Erfahrung als Grafikdesignerin und Art Director in Werbeagenturen schon alles gestaltet habe, was man sich nur vorstellen kann.</p>
+                            <p class="mb-0">Stattdessen möchte ich aber Folgendes erzählen: Mein Name ist Madeleine Lachmann. Ich bin Grafikdesignerin. Denn ich liebe es, zu gestalten. Deshalb habe ich beschlossen, meine eigene Gestaltungswerkstatt zu eröffnen. Um das zu tun, was ich gut kann, was ich liebe, was ich weitergeben möchte.</p>
+                            <div class="float-image hide-on-mobile"><img decoding="async" src="http://neu.design-made.at/wp-content/uploads/2024/01/Madeleine-163-min-scaled-aspect-ratio-466-660.jpg" alt></div>
+                            <p class="mb-0">Als Ein-Frau-Unterehmen biete ich das volle Programm an Agenturleistungen, von der Recherche bis zum fertigen Design. Immer mit vollem Einsatz und voller Leidenschaft.<br> Design made with love. </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="about-bilder content-section">
+            <div class="row" style="width: 978px;">
+                <div class="hide-on-desktop m-0">
+                    <p class="continua rosa name m-0">Madeleine Lachmann</p>
+                </div>
+                <div class="hide-on-desktop">
+                    <img decoding="async" class="img-fluid" src="http://neu.design-made.at/wp-content/uploads/2024/01/Madeleine-163-min-scaled-aspect-ratio-466-660.jpg" alt>
+                </div>
+                <div class="col-lg-7 col-md-6">
+                    <img decoding="async" class="img-fluid" src="http://neu.design-made.at/wp-content/uploads/2024/01/Madeleine-63-min-scaled.jpg" alt>
+                </div>
+                <div class="col-lg-5 col-md-6 right">
+                    <div class="row">
+                        <div class="col-lg-7 col-md-6">
+                            <img decoding="async" class="img-fluid" src="http://neu.design-made.at/wp-content/uploads/2024/01/Madeleine-149-min-scaled-aspect-ratio-250-233-2.jpg" alt>
+                        </div>
+                        <div class="col-lg-5 col-md-6 hide-on-mobile">
+                            <p class="continua rosa name">Madeleine Lachmann</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Über mich Ende -->
+</body>
+</html>
